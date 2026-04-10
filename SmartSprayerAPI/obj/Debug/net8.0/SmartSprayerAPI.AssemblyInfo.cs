b6eb6969d9a70111bcb1318a9cf4de22473e5c4b
Carlos Hernández Aguilar <karl.aguilar@hotmail.com>
2026-04-09 20:35:35 -0600

    Removing example of WeatherForecast

--- SmartSprayerAPI/obj/Debug/net8.0/SmartSprayerAPI.AssemblyInfo.cs
+++ SmartSprayerAPI/obj/Debug/net8.0/SmartSprayerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartSprayerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2582ec6576d710151498f294205dba008bc597")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartSprayerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartSprayerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
