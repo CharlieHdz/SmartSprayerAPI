bc6332c6d76c37105a8f97444484b0018e144f83
Carlos Hernández Aguilar <karl.aguilar@hotmail.com>
2026-04-15 17:03:41 -0600

    Going Live: Migrating dbc to Azure SQL

--- SmartSprayerAPI/Program.cs
+++ SmartSprayerAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-                                            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SmartSprayerDB;Trusted_Connection=True;"));
+                                            options.UseSqlServer("Server=tcp:smartsprayer-sql-us.database.windows.net,1433;Initial Catalog=SmartSprayerDB;Persist Security Info=False;User ID=sqladmin;Password=SprayerAdmin!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
 builder.Services.AddControllers();
 builder.Services.AddScoped<SensorService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
